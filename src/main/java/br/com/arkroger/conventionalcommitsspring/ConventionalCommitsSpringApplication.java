7363637b383616ae78214881c3aa63640f82ab61
Rogério <souza.rogeriof@gmail.com>
2022-06-11 11:30:34 -0300

    Initial commit

---
+++ src/main/java/br/com/arkroger/conventionalcommitsspring/ConventionalCommitsSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.arkroger.conventionalcommitsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConventionalCommitsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConventionalCommitsSpringApplication.class, args);
+	}
+
+}
